9de545347d79d2e6163201d8a8ccdcd2ca03f6d4
Shlomi Haver <shlomi.haver@booking.com>
2019-08-10 20:12:26 +0300

    spliting game and board

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,142 +3,24 @@ import Card from './card';
 import Snackbar from "@material-ui/core/Snackbar/Snackbar";
 import MySnackbarContentWrapper from "./snack-bar";
 
-const stompClient = require('./websocket-listener');
-var dummyCards = [];
-for (var i = 0; i < 52; i++) {
-    dummyCards.push({"i": i, cardName: "Cblue_back", selected: false});
-}
+
 export default function Board(props) {
-    const [isConnected, setIsConnected] = React.useState(false);
-    const [isOpenBottom, setIsOpenBottom] = React.useState(false);
-    const [isOpenTop, setIsOpenTop] = React.useState(false);
     const [cards, setCards] = React.useState([]);
     const cardsRef = React.useRef(cards);
-    const [infoMessage, setInfoMessage] = React.useState("");
-    const [topInfoMessage, setTopInfoMessage] = React.useState("");
 
-    //For updating of the ref
-    React.useEffect(() => {
-        cardsRef.current = cards;
-    }, [cards]);
 
-    function cardUpdate(response) {
-        var body = JSON.parse(response.body);
-        var data = JSON.parse(body.data);
-
-        if (body.command === "CardDraw") {
-            let cardId = data.cardId;
-            let selectedCard = data.selectedCard;
-            let cardName = getCardName(selectedCard.value, selectedCard.type);
-            let newCards = cardsRef.current.slice();
-            newCards[cardId] = Object.assign({}, newCards[cardId]);
-            newCards[cardId].cardName = cardName;
-            newCards[cardId].selected = true;
-            setCards(newCards);
-            let player = data.player;
-            updateMessage("bottom", player.name + " drawed the: " + selectedCard.valueName + " of " + selectedCard.type);
-        } else if (body.command === "TakeOne") {
-            let player = data.player;
-            updateMessage("top", "It's " + player.name + "'s turn now.");
-
-        } else if (body.command === "TakeTwo") {
-            let player = data.player;
-            updateMessage("top", player.name + "needs to pick 2 cards" +
-                " now.");
-        }
-    }
-
-    if (!isConnected) {
-        setCards(dummyCards);
-        stompClient.register([
-            {route: '/topic/draw', callback: cardUpdate},
-        ]);
-        setIsConnected(true);
-    }
-
-    const handleCloseInfoMessage = (type, event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-        if (type == "bottom") {
-            setIsOpenBottom(false);
-        } else {
-            setIsOpenTop(false);
-        }
-    };
-
-    const updateMessage = (type, message) => {
-        if (type == "bottom") {
-            setInfoMessage(message);
-            setIsOpenBottom(true);
-        } else {
-            setTopInfoMessage(message);
-            setIsOpenTop(true);
-        }
-    };
 
     return (
         <div className="board-container">
-            <Snackbar
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                }}
-                open={isOpenTop}
-                autoHideDuration={3000}
-                onClose={(e, r) => (handleCloseInfoMessage("top", e, r))}
-            >
-                <MySnackbarContentWrapper
-                    onClose={(e, r) => (handleCloseInfoMessage("top", e, r))}
-                    variant="info"
-                    message={topInfoMessage}
-                />
-            </Snackbar>
+
             <div className="board">
-                {cards.map(card => {
+                {props.deck.map(card => {
                     return (<Card key={card.i} cardId={card.i} cardName={card.cardName} drawFunc={props.drawCard}
                                   selected={card.selected}/>)
                 })}
 
             </div>
-            <Snackbar
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                }}
-                open={isOpenBottom}
-                autoHideDuration={2000}
-                onClose={(e, r) => (handleCloseInfoMessage("bottom", e, r))}
-            >
-                <MySnackbarContentWrapper
-                    onClose={(e, r) => (handleCloseInfoMessage("bottom", e, r))}
-                    variant="info"
-                    message={infoMessage}
-                />
-            </Snackbar>
+
         </div>
     );
-}
-
-
-function getCardName(value, type) {
-    switch (value) {
-        case 0:
-            value = "K";
-            break;
-        case 12:
-            value = "Q";
-            break;
-        case 11:
-            value = "J";
-            break;
-        case 1:
-            value = "A";
-            break;
-        default:
-            value = value.toString();
-    }
-
-    type = type.substr(0, 1);
-    return "C" + value + type;
 }
--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,3 +1,5 @@
+import Snackbar from "@material-ui/core/Snackbar/Snackbar";
+
 const React = require('react');
 const ReactDOM = require('react-dom');
 const axios = require('axios')
@@ -8,12 +10,29 @@ import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import queryString from 'query-string'
 // import Cookies from 'js-cookie';
 import Cookies from 'universal-cookie';
+import MySnackbarContentWrapper from "./snack-bar";
+
+const stompClient = require('./websocket-listener');
+var dummyCards = [];
 
 
 export default function Game(props) {
+    const [isConnected, setIsConnected] = React.useState(false);
+    const [isDeckUpdated, setIsDeckUpdated] = React.useState(false);
     let [gameId, setGameId] = React.useState(null);
     let [playerId, setPlayerId] = React.useState(null);
+    const [isOpenBottom, setIsOpenBottom] = React.useState(false);
+    const [isOpenTop, setIsOpenTop] = React.useState(false);
+    const [cards, setCards] = React.useState([]);
+    const cardsRef = React.useRef(cards);
+    const [infoMessage, setInfoMessage] = React.useState("");
+    const [topInfoMessage, setTopInfoMessage] = React.useState("");
+
     const cookies = new Cookies();
+    //For updating of the ref
+    React.useEffect(() => {
+        cardsRef.current = cards;
+    }, [cards]);
 
     const values = queryString.parse(window.location.search);
 
@@ -45,6 +64,60 @@ export default function Game(props) {
         }
     }
 
+    if (isDeckUpdated === false) {
+        //get the deck from server
+        for (var i = 0; i < 52; i++) {
+            dummyCards.push({"i": i, cardName: "Cblue_back", selected: false});
+        }
+        setCards(dummyCards);
+        setIsDeckUpdated(true)
+    }
+
+    if (!isConnected) {
+        setCards(dummyCards);
+        stompClient.register([
+            {route: '/topic/draw', callback: cardUpdate},
+        ]);
+        setIsConnected(true);
+    }
+
+    function cardUpdate(response) {
+        var body = JSON.parse(response.body);
+        var data = JSON.parse(body.data);
+
+        if (body.command === "CardDraw") {
+            let cardId = data.cardId;
+            let selectedCard = data.selectedCard;
+            let cardName = getCardName(selectedCard.value, selectedCard.type);
+            let newCards = cardsRef.current.slice();
+            newCards[cardId] = Object.assign({}, newCards[cardId]);
+            newCards[cardId].cardName = cardName;
+            newCards[cardId].selected = true;
+            setCards(newCards);
+            let player = data.player;
+            updateMessage("bottom", player.name + " drawed the: " + selectedCard.valueName + " of " + selectedCard.type);
+        } else if (body.command === "TakeOne") {
+            let player = data.player;
+            updateMessage("top", "It's " + player.name + "'s turn now.");
+
+        } else if (body.command === "TakeTwo") {
+            let player = data.player;
+            updateMessage("top", player.name + "needs to pick 2 cards" +
+                " now.");
+        }
+    }
+
+    const updateMessage = (type, message) => {
+        if (type == "bottom") {
+            setInfoMessage(message);
+            setIsOpenBottom(true);
+        } else {
+            setTopInfoMessage(message);
+            setIsOpenTop(true);
+        }
+    };
+
+
     function drawCard(cardId) {
         const pResponse = axios({
             method: "POST",
@@ -64,10 +137,75 @@ export default function Game(props) {
         return pResponse;
     }
 
+    const handleCloseInfoMessage = (type, event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        if (type == "bottom") {
+            setIsOpenBottom(false);
+        } else {
+            setIsOpenTop(false);
+        }
+    };
+
     return (
         //TODO:: pass deck here
         <div>
-            <Board drawCard={drawCard} deck={null} me={playerId}/>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                open={isOpenTop}
+                autoHideDuration={3000}
+                onClose={(e, r) => (handleCloseInfoMessage("top", e, r))}
+            >
+                <MySnackbarContentWrapper
+                    onClose={(e, r) => (handleCloseInfoMessage("top", e, r))}
+                    variant="info"
+                    message={topInfoMessage}
+                />
+            </Snackbar>
+            <Board drawCard={drawCard} deck={cards} me={playerId}/>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                open={isOpenBottom}
+                autoHideDuration={2000}
+                onClose={(e, r) => (handleCloseInfoMessage("bottom", e, r))}
+            >
+                <MySnackbarContentWrapper
+                    onClose={(e, r) => (handleCloseInfoMessage("bottom", e, r))}
+                    variant="info"
+                    message={infoMessage}
+                />
+            </Snackbar>
         </div>
     )
 };
+
+
+
+function getCardName(value, type) {
+    switch (value) {
+        case 0:
+            value = "K";
+            break;
+        case 12:
+            value = "Q";
+            break;
+        case 11:
+            value = "J";
+            break;
+        case 1:
+            value = "A";
+            break;
+        default:
+            value = value.toString();
+    }
+
+    type = type.substr(0, 1);
+    return "C" + value + type;
+}
